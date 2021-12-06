5ba9a54e069e89a22de09d6237079ee2dddb597d
ashwg <ashwg@cisco.com>
2021-12-06 20:35:11 +0530

    Fetching public ip from int cluster node (#92)

--- dsb-calling-app/server/docker/env.sh
+++ dsb-calling-app/server/docker/env.sh
@@ -4,6 +4,11 @@ echo "BEGIN env.sh"
 export JAVA_VERSION=11
 export TOMCAT_VERSION=9
 export TOMCAT_http_port=8080
+export temp=$(ip -4 addr show eth2 2>/dev/null | grep -oP '(?<=inet\s)\d+(.\d+){3}')
+if [ ! -z "$temp" ]
+then
+  POD_IP=$temp
+fi
 #if [ -f "/test/server.pem" ]; then
 #    echo "IMPORTing public cert to cacerts"
 #    export JHOME="/usr/lib/jvm/openjdk-8u222-b10-jre"
