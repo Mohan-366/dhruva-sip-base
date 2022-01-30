681c9d471f11c120413afbc1b2040517bad45d82
ankabane <ankabane@cisco.com>
2022-01-31 02:39:15 +0530

    connection metric task added

--- dsb-common/src/main/java/com/cisco/dsb/common/sip/jain/channelCache/DsbJainSipTLSMessageProcessor.java
+++ dsb-common/src/main/java/com/cisco/dsb/common/sip/jain/channelCache/DsbJainSipTLSMessageProcessor.java
@@ -2,6 +2,9 @@ package com.cisco.dsb.common.sip.jain.channelCache;
 
 import com.cisco.dsb.common.config.sip.CommonConfigurationProperties;
 import com.cisco.dsb.common.executor.DhruvaExecutorService;
+import com.cisco.dsb.common.service.MetricService;
+import com.cisco.dsb.common.transport.Connection;
+import com.cisco.dsb.common.util.log.event.Event;
 import com.google.common.base.Preconditions;
 import gov.nist.javax.sip.SipStackImpl;
 import gov.nist.javax.sip.stack.ConnectionOrientedMessageChannel;
@@ -11,11 +14,14 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Collection;
 import lombok.CustomLog;
+import lombok.Getter;
 
 @CustomLog
 public class DsbJainSipTLSMessageProcessor extends TLSMessageProcessor
     implements MessageChannelCache {
   private final StartStoppable keepAliveTimerTask;
+  private final StartStoppable connectionMetricTask;
+  @Getter private MetricService metricService;
 
   /**
    * Constructor.
@@ -29,23 +35,39 @@ public class DsbJainSipTLSMessageProcessor extends TLSMessageProcessor
       SIPTransactionStack sipStack,
       int port,
       CommonConfigurationProperties sipProperties,
-      DhruvaExecutorService executorService) {
+      DhruvaExecutorService executorService,
+      MetricService metricService) {
     super(ipAddress, sipStack, port);
     Preconditions.checkNotNull(sipProperties);
     keepAliveTimerTask = new KeepAliveTimerTask(this, sipProperties, executorService);
+    this.metricService = metricService;
+    connectionMetricTask = new ConnectionMetricRunnable(this, this.metricService, executorService);
   }
 
   /** Start our thread. */
   public void start() throws IOException {
-    logger.info("TCP message processor thread for stack {} starting", getStackName());
-    super.start();
-    keepAliveTimerTask.start();
+    logger.info("TLS message processor thread for stack {} starting", getStackName());
+    try {
+      super.start();
+      keepAliveTimerTask.start();
+      connectionMetricTask.start();
+    } catch (Exception ex) {
+      logger.error("Error starting TLS message processor");
+      logger.emitEvent(
+              Event.EventType.CONNECTION,
+              Event.EventSubType.TCPCONNECTION,
+              Event.ErrorType.ConnectionError,
+              ex.getMessage(),
+              null,
+              ex);
+      throw ex;
+    }
   }
 
   /** Stop method. */
   public void stop() {
     keepAliveTimerTask.stop();
-    logger.info("TCP message processor thread for stack {} stopping", getStackName());
+    logger.info("TLS message processor thread for stack {} stopping", getStackName());
     super.stop();
   }
 
@@ -58,6 +80,14 @@ public class DsbJainSipTLSMessageProcessor extends TLSMessageProcessor
   public Collection<ConnectionOrientedMessageChannel> getIncomingMessageChannels() {
     return incomingMessageChannels.values();
   }
+
+  @Override
+  protected synchronized void remove(ConnectionOrientedMessageChannel messageChannel) {
+    metricService.emitConnectionMetrics(Event.DIRECTION.OUT.toString(), messageChannel, Connection.STATE.DISCONNECTED.toString());
+    super.remove(messageChannel);
+    logger.debug("Connection removed from message processor");
+  }
+
 
   @Override
   public String getStackName() {
